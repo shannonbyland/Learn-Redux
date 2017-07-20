1402fed85765ca5b8d2a8bf1682d9bac251ccf43
Shannon Byland <shannonbyland@gmail.com>
2017-07-19 18:34:40 -0600

    added react-router paths

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -3,5 +3,18 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 import Main from './components/Main.js';
+import Single from './components/Single.js';
+import PhotoGrid from './components/PhotoGrid.js';
 
-render(<Main />, document.getElementById('root'));
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+render(router, document.getElementById('root'));

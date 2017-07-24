5e1ee751a966c97bc6491564695aae6928a7bb36
Shannon Byland <shannonbyland@gmail.com>
2017-07-24 11:19:54 -0600

    added photo component

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,11 +1,16 @@
 import React from 'react';
+
 import { render } from 'react-dom';
+
+// Import css
 import css from './styles/style.styl';
 
-import Main from './components/Main.js';
-import Single from './components/Single.js';
-import PhotoGrid from './components/PhotoGrid.js';
+// Import Components
+import App from './components/App';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
+// import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
@@ -13,7 +18,7 @@ import store, { history } from './store';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid}></IndexRoute>
         <Route path="/view/:postId" component={Single}></Route>
       </Route>

4cdee8a498dccb7648cde667c6accc1987639972
Shannon Byland <shannonbyland@gmail.com>
2017-07-24 12:27:36 -0600

    added comments component

--- 13/components/Single.js
+++ 13/components/Single.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    const post = this.props.posts[i];
+
     return (
       <div className="single-photo">
-      Im the single
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }

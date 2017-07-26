c42dd36dfba199d95ca346bceaacab3f6f1a5f17
Shannon Byland <shannonbyland@gmail.com>
2017-07-25 20:52:06 -0600

    comments working

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -7,7 +7,10 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button
+            className="remove-comment"
+            onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;
+          </button>
         </p>
       </div>
     )
@@ -17,7 +20,9 @@ const Comments = React.createClass({
     e.preventDefault();
     const { postId } = this.props.params;
     const author = this.refs.author.value;
-    const comments = this.refs.comments.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
 
   render() {
